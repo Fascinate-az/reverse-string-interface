094c9ef96f0a8f61b905f831623682b0621fe091
Fascinate <eddo92@yahoo.it>
2025-04-28 16:38:49 +0200

    Create reversestring.cs

---
+++ reversestring.cs
@@ -0,0 +1,62 @@
+class TransformStringFinal: IStringTransformation
+{
+    public string Transform(string input)
+    {
+        List<IStringTransformation> listaTrasformaStringa = new List<IStringTransformation>()
+        {
+            new UpperCaseTransformer(),
+            new AddWorldTransformer(),
+            new ReverseTransformer(),
+            new AddExclamationTransformer(),
+            
+        };
+
+        var risultato = input;
+
+        foreach (var item in listaTrasformaStringa)
+        {
+            risultato = item.Transform(risultato);
+        }
+        return risultato;
+    }
+}
+
+class UpperCaseTransformer: IStringTransformation
+{
+    public string Transform(string input)
+    {
+        return input.ToUpper();
+    }
+}
+
+class ReverseTransformer : IStringTransformation
+{
+    public string Transform(string input)
+    {
+        char [] charArray = input.ToCharArray();
+        string reversedString = "";
+
+        for (int i = charArray.Length - 1; i >= 0; i--)
+        {
+            reversedString += charArray[i];  
+        }
+
+        return reversedString;
+    }
+}
+
+class AddExclamationTransformer: IStringTransformation
+{
+    public string Transform(string input)
+    {
+        return "!" + input;
+    }
+}
+
+class AddWorldTransformer : IStringTransformation
+{
+    public string Transform(string input)
+    {
+        return $"{input} WORLD";
+    }
+}
